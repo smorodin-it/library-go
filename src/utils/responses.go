8c5498040b47d99349224de20cd0f6364d819c95
Smorodin I.T <smorodin.it@gmail.com>
2022-10-01 22:32:38 +0300

    dev

---
+++ src/utils/responses.go
@@ -0,0 +1,15 @@
+package utils
+
+type ErrorResponse struct {
+	FailedField string `json:"failedField"`
+	Tag         string `json:"tag"`
+	Value       string `json:"value"`
+}
+
+type ResponseAdd struct {
+	Id string `json:"id"`
+}
+
+type ResponseStatus struct {
+	Status bool `json:"status"`
+}
